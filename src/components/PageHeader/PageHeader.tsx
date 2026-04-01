76efcce22e7a0c2eb9a83d1e1ad255d77a7d8c1a
senaev <senaev@yandex-team.ru>
2026-04-01 16:37:59 +0200

    default message

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -1,26 +1,27 @@
 import { useNavigate } from "react-router-dom";
-import "./PageHeader.css";
 import { ROUTES } from "../../const/ROUTES";
+import "./PageHeader.css";
 
 export function PageHeader({
-    children,
-    homeButtonIcon,
+  children,
+  homeButtonIcon,
 }: {
-    children: React.ReactNode;
-    homeButtonIcon: React.ReactNode;
+  children: React.ReactNode;
+  homeButtonIcon: React.ReactNode;
 }) {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <header className="PageHeader">
-            <button
-                onClick={() => {
-                    navigate(ROUTES.home);
-                }}
-            >
-                {homeButtonIcon}
-            </button>
-            {children}
-        </header>
-    );
+  return (
+    <header className="PageHeader">
+      <button
+        type="button"
+        onClick={() => {
+          navigate(ROUTES.home);
+        }}
+      >
+        {homeButtonIcon}
+      </button>
+      {children}
+    </header>
+  );
 }
